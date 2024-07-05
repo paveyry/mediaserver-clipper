86c59ba526f8b16f79794c74062a38272e80c638
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2024-07-05 22:10:12 +0100

    src: ffprobe: add ffprobe util functions

---
+++ src/ffprobe.rs
@@ -0,0 +1,94 @@
+use std::any;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+use anyhow::Error;
+use serde::Deserialize;
+use serde_json::{Number, Value};
+
+#[derive(Debug, Deserialize)]
+struct FFProbeInfo {
+    streams: Vec<StreamInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+struct StreamInfo {
+    codec_type: String,
+    codec_name: String,
+    index: u32,
+    tags: Option<StreamTags>,
+}
+
+#[derive(Default, Debug, Deserialize)]
+struct StreamTags {
+    language: Option<String>,
+    title: Option<String>,
+}
+
+fn type_err_msg(typ: &str) -> anyhow::Error {
+    Error::msg("ffprobe json should be of type {typ}")
+}
+
+fn key_err_msg(key: &str) -> anyhow::Error {
+    Error::msg(format!("{key} information is missing from ffprobe output"))
+}
+
+pub fn list_audio_tracks(file_path: PathBuf) -> anyhow::Result<Vec<String>> {
+    // TODO: return a dict and avoid string split to extract value
+
+    let output = Command::new("ffprobe")
+        .args(["-print_format", "json"])
+        .args(["-show_streams"])
+        .args(["-show_format"])
+        .args(["-select_streams", "a"])
+        .args([file_path])
+        .output()?;
+
+    let data: FFProbeInfo = serde_json::from_slice(&output.stdout)?;
+
+    let mut audio_tracks = Vec::new();
+    for t in data.streams {
+        if t.codec_type != "audio" {
+            continue;
+        }
+        let index = t.index;
+        let tags = t.tags.unwrap_or_default();
+        let lang = tags.language.unwrap_or_default();
+        let title = tags.title.unwrap_or_default();
+        let codec = t.codec_name;
+
+        audio_tracks.push(format!("{index}:'{title}' ({lang} - {codec})"));
+    }
+
+    Ok(audio_tracks)
+}
+
+pub fn list_subtitle_tracks(file_path: PathBuf) -> anyhow::Result<Vec<String>> {
+    // TODO: return a dict and avoid string split to extract value
+
+    let output = Command::new("ffprobe")
+        .args(["-print_format", "json"])
+        .args(["-show_streams"])
+        .args(["-show_format"])
+        .args(["-select_streams", "s"])
+        .args([file_path])
+        .output()?;
+
+    let data: FFProbeInfo = serde_json::from_slice(&output.stdout)?;
+
+    let mut subtitle_tracks = Vec::new();
+    for t in data.streams {
+        if t.codec_type != "subtitle" {
+            continue;
+        }
+        let index = t.index;
+        let tags = t.tags.unwrap_or_default();
+        let lang = tags.language.unwrap_or_default();
+        let title = tags.title.unwrap_or_default();
+        let codec = t.codec_name;
+
+        subtitle_tracks.push(format!("{index}:'{title}' ({lang} - {codec})"));
+    }
+
+    Ok(subtitle_tracks)
+}
