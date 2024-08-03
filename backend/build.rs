d9b7b8865af6f7d262ca753d82f79bf6cb490bb9
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2024-08-03 02:24:40 +0200

    src: backend: mbuild.rs: log trunk build output

--- backend/build.rs
+++ backend/build.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 const UI_DIR: &str = "../ui";
 
 fn main() {
     println!("cargo:rerun-if-changed={UI_DIR}/src");
+    println!("cargo:rerun-if-changed={UI_DIR}/Cargo.toml");
     println!("cargo:rerun-if-changed={UI_DIR}/index.html");
     build_frontend(UI_DIR);
 }
@@ -12,6 +13,8 @@ fn main() {
 fn build_frontend<P: AsRef<Path>>(source: P) {
     Command::new("trunk")
         .args(["build", "--dist", "../backend/dist"])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .current_dir(source.as_ref())
         .status()
         .expect("Failed to build Frontend");
